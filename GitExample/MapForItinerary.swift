ee0507ed0fed42971b60cc0df2e19dcab6106bb7
Jayant <jayants@bdminfo.tech>
2017-04-12 16:35:15 +0530

    add map feature

---
+++ GitExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitExample
+//
+//  Created by Praveen Balpande on 4/12/17.
+//  Copyright © 2017 Praveen Balpande. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
